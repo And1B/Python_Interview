c69726982b9acc1502be4318186d0dcba9e0a6ef
Andi <28365760+And1B@users.noreply.github.com>
2021-12-08 19:37:52 +0100

    Update check_lists.py
    Adding Constants

--- check_lists.py
+++ check_lists.py
@@ -1,9 +1,9 @@
-from Methods import generate_data, read_data, check_differences
+from Methods import generate_data, read_data, check_differences, SPECIFIEDFILE, MEASUREDFILE
 
 if __name__ == '__main__':
-    # generate_data('SpecifiedList.txt')
-    # generate_data('MeasuredList.txt')
-    specifiedList = read_data('SpecifiedList.txt')
-    measuredList = read_data('MeasuredList.txt')
+    # generate_data(SPECIFIEDFILE)
+    # generate_data(MEASUREDFILE)
+    specifiedList = read_data(SPECIFIEDFILE)
+    measuredList = read_data(MEASUREDFILE)
     check_differences(measuredList, specifiedList)
 
